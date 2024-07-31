b1f462db9f3cb51400b2f164230751759b22dafe
Siil777 <pavel.ivanov.thk@gmail.com>
2024-07-31 09:40:26 +0300

    feat: function convert num to string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function toReadable (number) {
-  
+    if (number === 0) return 'zero';
+    const belowTwenty = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const aboveHundred = ['hundred'];
+    function convert(number) {
+        if (number < 20) return belowTwenty[number];
+        if (number < 100) return tens[Math.floor(number / 10)] + (number % 10 ? ' ' + belowTwenty[number % 10] : '');
+        if (number < 1000) return belowTwenty[Math.floor(number / 100)] + ' ' + aboveHundred[0] + (number % 100 ? ' ' + convert(number % 100) : '');
+    }
+    return convert(number);
 }
